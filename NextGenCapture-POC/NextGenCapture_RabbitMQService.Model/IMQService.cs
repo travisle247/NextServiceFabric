084886dd37bf60eb0fa5ab2484b277435cad44d9
travisle247 <travisle247@hotmail.com>
2018-02-26 20:15:28 -0600

    Check in

---
+++ NextGenCapture-POC/NextGenCapture_RabbitMQService.Model/IMQService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.ServiceFabric.Services.Remoting;
+using Microsoft.ServiceFabric.Services.Remoting.FabricTransport;
+using NextGenCapture_Common.Model.Export;
+using System;
+using System.Threading.Tasks;
+
+[assembly: FabricTransportServiceRemotingProvider(RemotingListener = RemotingListener.V2Listener, RemotingClient = RemotingClient.V2Client)]
+
+namespace NextGenCapture_RabbitMQService.Model
+{
+    public interface IMQService : IService
+    {
+        Task<MessageResponse> SendToRabbitMessageQueue(STPBatch batch);
+        Task<MessageResponse> ReceiveFromRabbitMessageQueue();
+    }
+}
---
+++ NextGenCapture-POC/NextGenCapture_RabbitMQService.Model/MessageResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NextGenCapture_RabbitMQService.Model
+{
+    public class MessageResponse
+    {
+        public bool Success { get; set; }
+    }
+}
